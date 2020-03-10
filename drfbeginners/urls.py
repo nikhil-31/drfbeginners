ce5adc44876dab33306a1f84117e0ab1db31f3f4
nikhil <nikhil@urbanpiper.com>
2020-03-10 22:59:48 +0530

    Added CRUD operations with DRF framework

--- drfbeginners/urls.py
+++ drfbeginners/urls.py
@@ -1,11 +1,13 @@
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
-from posts.views import PostView
+
+from posts.views import post_list, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/posts/', PostView.as_view(), name='post-list'),
+    # path('api/posts/', PostView.as_view(), name='post-list'),
     path('quickstart/', include('quickstart.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/posts/', post_list, name='post-list'),
+    path('api/posts/<int:pk>', post_detail, name='post-detail')
 ]
--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+
 from .models import Post
 
-# Register your models here.
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'custom_id', 'category', 'publish_date', 'last_published')
+
+
+admin.site.register(Post, PostAdmin)
