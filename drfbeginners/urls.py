12c1053d706553ed4d39fc5fc4c1c9d59fb06974
nikhil <nikhil@urbanpiper.com>
2020-03-15 01:11:59 +0530

    Added routers

--- drfbeginners/urls.py
+++ drfbeginners/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from posts.views import PostDetailView, PostDestroyView, PostListView, OwnerDetailView, CommentDetailView
+from posts.views import OwnerDetailView, CommentDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/posts/<pk>/', PostDetailView.as_view(), name='post-list'),
-    path('api/posts/<pk>/delete/', PostDestroyView.as_view(), name='post-list'),
-    path('api/posts/', PostListView.as_view(), name='post-list'),
     path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('posts.urls')),
+    # path('api/posts/<pk>/', PostDetailView.as_view(), name='post-list'),
+    # path('api/posts/<pk>/delete/', PostDestroyView.as_view(), name='post-list'),
+    # path('api/posts/', PostListView.as_view(), name='post-list'),
     path('api/owner/<pk>/', OwnerDetailView.as_view(), name='owner-detail'),
     path('api/comment/<pk>/', CommentDetailView.as_view(), name='comment-detail'),
     # path('quickstart/', include('quickstart.urls')),
--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework import viewsets
 from rest_framework.parsers import JSONParser
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
@@ -127,3 +128,9 @@ class OwnerDetailView(generics.RetrieveAPIView):
 class CommentDetailView(generics.RetrieveAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AllowAny,)
